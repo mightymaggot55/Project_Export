d849821b342e9c3b48a7d028aad7176167bafb94
mightymaggot55 <48097201+mightymaggot55@users.noreply.github.com>
2022-02-23 17:02:07 +0000

    Wednesday 23/02/2022
    >Attempted to extend ticket system
    >Tried to add in inheritance from a GUI class, almost there, just KWARGS doesn't like TKINTER Label format

--- ChessProject_02.py
+++ ChessProject_02.py
@@ -1,50 +1,127 @@
 from tkinter import *
 from tkinter.ttk import *
 
-TYPE = ['PAWN', 'ROOK', 'KNIGHT', 'BISHOP', 'QUEEN', 'KING']
+CHESS_PIECE = ['PAWN', 'ROOK', 'KNIGHT', 'BISHOP', 'QUEEN', 'KING']
 STATUS = ['ALIVE', 'DEAD']
 TEAM = ['RED', 'BLACK']
 Squares = []
 start_window_size = "600x650"
 isDrawn = False
+Square_arr = {     'A1' : [100,500,150,450],
+                   'A2' : [100,450,150,400],
+                   'A3' : [100,400,150,350],
+                   'A4' : [100,350,150,300],
+                   'A5' : [100,300,150,250],
+                   'A6' : [100,250,150,200],
+                   'A7' : [100,200,150,150],
+                   'A8' : [100,150,150,100],
+                   'B1' : [150,500,200,450],
+                   'B2' : [150,450,200,400],
+                   'B3' : [150,400,200,350],
+                   'B4' : [150,350,200,300],
+                   'B5' : [150,300,200,250],
+                   'B6' : [150,250,200,200],
+                   'B7' : [150,200,200,150],
+                   'B8' : [150,150,200,100],
+                   'C1' : [200,500,250,450],
+                   'C2' : [200,450,250,400],
+                   'C3' : [200,400,250,350],
+                   'C4' : [200,350,250,300],
+                   'C5' : [200,300,250,250],
+                   'C6' : [200,250,250,200],
+                   'C7' : [200,200,250,150],
+                   'C8' : [200,150,250,100],
+                   'D1' : [250,500,300,450],
+                   'D2' : [250,450,300,400],
+                   'D3' : [250,400,300,350],
+                   'D4' : [250,350,300,300],
+                   'D5' : [250,300,300,250],
+                   'D6' : [250,250,300,200],
+                   'D7' : [250,200,300,150],
+                   'D8' : [250,150,300,100],
+                   'E1' : [300,500,350,450],
+                   'E2' : [300,450,350,400],
+                   'E3' : [300,400,350,350],
+                   'E4' : [300,350,350,300],
+                   'E5' : [300,300,350,250],
+                   'E6' : [300,250,350,200],
+                   'E7' : [300,200,350,150],
+                   'E8' : [300,150,350,100],
+                   'F1' : [350,500,400,450],
+                   'F2' : [350,450,400,400],
+                   'F3' : [350,400,400,350],
+                   'F4' : [350,350,400,300],
+                   'F5' : [350,300,400,250],
+                   'F6' : [350,250,400,200],
+                   'F7' : [350,200,400,150],
+                   'F8' : [350,150,400,100],
+                   'G1' : [400,500,450,450],
+                   'G2' : [400,450,450,400],
+                   'G3' : [400,400,450,350],
+                   'G4' : [400,350,450,300],
+                   'G5' : [400,300,450,250],
+                   'G6' : [400,250,450,200],
+                   'G7' : [400,200,450,150],
+                   'G8' : [400,150,450,100],
+                   'H1' : [450,500,500,450],
+                   'H2' : [450,450,500,400],
+                   'H3' : [450,400,500,350],
+                   'H4' : [450,350,500,300],
+                   'H5' : [450,300,500,250],
+                   'H6' : [450,250,500,200],
+                   'H7' : [450,200,500,150],
+                   'H8' : [450,150,500,100]
+                   }
 
 class ChessPiece:
-    def __init__(self, name, startingPosition, type, status, allowedMovement, currentPosition, team):
+    def __init__(self, name, startingPosition, piece, status, allowedMovement, currentPosition, team):
         self.name = name
         self.startingPosition = startingPosition
-        self.type = type
+        self.piece = piece
         self.status = status
         self.allowedMovement = allowedMovement
         self.currentPosition = currentPosition
         self.team = team
 
 
-class Square:
-    def __init__(self, x0, y0, x1, y1):
-        self.x0 = x0
-        self.y0 = y0
-        self.x1 = x1
-        self.y1 = y1
+#class Square:
+#    def __init__(self, x0, y0, x1, y1):
+#        self.x0 = x0
+#        self.y0 = y0
+#        self.x1 = x1
+#        self.y1 = y1
+
+class Square(dict):
+    def __init__(self, default=None):
+        dict.__init__(self)
+        self.default = default
+
+    def __getitem__(self, key):
+        try:
+            return dict.__getitem__(self, key)
+        except KeyError:
+            return self.default
+
 
 
 def Init_Pieces():
     squares = Square()
-    P1 = ChessPiece('Pawn_01', '', TYPE[0], STATUS[0], '', '', TEAM[0])
-    P2 = ChessPiece('Pawn_02', '', TYPE[0], STATUS[0], '', '', TEAM[0])
-    P3 = ChessPiece('Pawn_03', '', TYPE[0], STATUS[0], '', '', TEAM[0])
-    P4 = ChessPiece('Pawn_04', '', TYPE[0], STATUS[0], '', '', TEAM[0])
-    P5 = ChessPiece('Pawn_05', '', TYPE[0], STATUS[0], '', '', TEAM[0])
-    P6 = ChessPiece('Pawn_06', '', TYPE[0], STATUS[0], '', '', TEAM[0])
-    P7 = ChessPiece('Pawn_07', '', TYPE[0], STATUS[0], '', '', TEAM[0])
-    P8 = ChessPiece('Pawn_08', '', TYPE[0], STATUS[0], '', '', TEAM[0])
-    R1 = ChessPiece('Rook_01', '', TYPE[1], STATUS[0], '', '', TEAM[0])
-    R2 = ChessPiece('Rook_02', '', TYPE[2], STATUS[0], '', '', TEAM[0])
-    K1 = ChessPiece('Knight_01', '', TYPE[3], STATUS[0], '', '', TEAM[0])
-    K2 = ChessPiece('Knight_02', '', TYPE[3], STATUS[0], '', '', TEAM[0])
-    B1 = ChessPiece('Bishop_01', '', TYPE[4], STATUS[0], '', '', TEAM[0])
-    B2 = ChessPiece('Bishop_02', '', TYPE[4], STATUS[0], '', '', TEAM[0])
-    Q1 = ChessPiece('Queen', '', TYPE[5], STATUS[0], '', '', TEAM[0])
-    KING1 = ChessPiece('King', '', TYPE[6], STATUS[0], '', '', TEAM[0])
+    P1 = ChessPiece('Pawn_01', '', CHESS_PIECE[0], STATUS[0], '', '', TEAM[0])
+    P2 = ChessPiece('Pawn_02', '', CHESS_PIECE[0], STATUS[0], '', '', TEAM[0])
+    P3 = ChessPiece('Pawn_03', '', CHESS_PIECE[0], STATUS[0], '', '', TEAM[0])
+    P4 = ChessPiece('Pawn_04', '', CHESS_PIECE[0], STATUS[0], '', '', TEAM[0])
+    P5 = ChessPiece('Pawn_05', '', CHESS_PIECE[0], STATUS[0], '', '', TEAM[0])
+    P6 = ChessPiece('Pawn_06', '', CHESS_PIECE[0], STATUS[0], '', '', TEAM[0])
+    P7 = ChessPiece('Pawn_07', '', CHESS_PIECE[0], STATUS[0], '', '', TEAM[0])
+    P8 = ChessPiece('Pawn_08', '', CHESS_PIECE[0], STATUS[0], '', '', TEAM[0])
+    R1 = ChessPiece('Rook_01', '', CHESS_PIECE[1], STATUS[0], '', '', TEAM[0])
+    R2 = ChessPiece('Rook_02', '', CHESS_PIECE[2], STATUS[0], '', '', TEAM[0])
+    K1 = ChessPiece('Knight_01', '', CHESS_PIECE[3], STATUS[0], '', '', TEAM[0])
+    K2 = ChessPiece('Knight_02', '', CHESS_PIECE[3], STATUS[0], '', '', TEAM[0])
+    B1 = ChessPiece('Bishop_01', '', CHESS_PIECE[4], STATUS[0], '', '', TEAM[0])
+    B2 = ChessPiece('Bishop_02', '', CHESS_PIECE[4], STATUS[0], '', '', TEAM[0])
+    Q1 = ChessPiece('Queen', '', CHESS_PIECE[5], STATUS[0], '', '', TEAM[0])
+    KING1 = ChessPiece('King', '', CHESS_PIECE[6], STATUS[0], '', '', TEAM[0])
 
 class GUI:
     def __init__(self, master_win):
@@ -63,71 +140,8 @@ class GUI:
         #Draws Board and all the squares (can be condensed into a for loop)
         canvas = Canvas()
         canvas.create_rectangle(100,500,500,100) #Outside Board
-        A8 = Square(100,150,150,100)
-        A7 = Square(100,200,150,150)
-        A6 = Square(100,250,150,200)
-        A5 = Square(100,300,150,250)
-        A4 = Square(100,350,150,300)
-        A3 = Square(100,400,150,350)
-        A2 = Square(100,450,150,400)
-        A1 = Square(100,500,150,450)
-        B8 = Square(150,150,200,100)
-        B7 = Square(150,200,200,150)
-        B6 = Square(150,250,200,200)
-        B5 = Square(150,300,200,250)
-        B4 = Square(150,350,200,300)
-        B3 = Square(150,400,200,350)
-        B2 = Square(150,450,200,400)
-        B1 = Square(150,500,200,450)
-        C8 = Square(200,150,250,100)
-        C7 = Square(200,200,250,150)
-        C6 = Square(200,250,250,200)
-        C5 = Square(200,300,250,250)
-        C4 = Square(200,350,250,300)
-        C3 = Square(200,400,250,350)
-        C2 = Square(200,450,250,400)
-        C1 = Square(200,500,250,450)
-        D8 = Square(250,150,300,100)
-        D7 = Square(250,200,300,150)
-        D6 = Square(250,250,300,200)
-        D5 = Square(250,300,300,250)
-        D4 = Square(250,350,300,300)
-        D3 = Square(250,400,300,350)
-        D2 = Square(250,450,300,400)
-        D1 = Square(250,500,300,450)
-        E8 = Square(300,150,350,100)
-        E7 = Square(300,200,350,150)
-        E6 = Square(300,250,350,200)
-        E5 = Square(300,300,350,250)
-        E4 = Square(300,350,350,300)
-        E3 = Square(300,400,350,350)
-        E2 = Square(300,450,350,400)
-        E1 = Square(300,500,350,450)
-        F8 = Square(350,150,400,100)
-        F7 = Square(350,200,400,150)
-        F6 = Square(350,250,400,200)
-        F5 = Square(350,300,400,250)
-        F4 = Square(350,350,400,300)
-        F3 = Square(350,400,400,350)
-        F2 = Square(350,450,400,400)
-        F1 = Square(350,500,400,450)
-        G8 = Square(400,150,450,100)
-        G7 = Square(400,200,450,150)
-        G6 = Square(400,250,450,200)
-        G5 = Square(400,300,450,250)
-        G4 = Square(400,350,450,300)
-        G3 = Square(400,400,450,350)
-        G2 = Square(400,450,450,400)
-        G1 = Square(400,500,450,450)
-        H8 = Square(450,150,500,100)
-        H7 = Square(450,200,500,150)
-        H6 = Square(450,250,500,200)
-        H5 = Square(450,300,500,250)
-        H4 = Square(450,350,500,300)
-        H3 = Square(450,400,500,350)
-        H2 = Square(450,450,500,400)
-        H1 = Square(450,500,500,450)
-        Squares = [A1, A2, A3, A4, A5, A6, A7, A8, B1, B2, B3, B4, B5, B6, B7, B8, C1, C2, C3, C4, C5, C6, C7, C8, D1, D2, D3, D4, D5, D6, D7, D8, E1, E2, E3, E4, E5, R6, E7, E8, F1, F2, F3, F4, F5, F6, F7, F8, G1, G2, G3, G4, G5, G6, G7, G8, H1, H2, H3, H4, H5, H6, H7, H8]
+
+      #  Squares = [A1, A2, A3, A4, A5, A6, A7, A8, B1, B2, B3, B4, B5, B6, B7, B8, C1, C2, C3, C4, C5, C6, C7, C8, D1, D2, D3, D4, D5, D6, D7, D8, E1, E2, E3, E4, E5, E6, E7, E8, F1, F2, F3, F4, F5, F6, F7, F8, G1, G2, G3, G4, G5, G6, G7, G8, H1, H2, H3, H4, H5, H6, H7, H8]
         print(Squares)
         A_Label = Label(canvas, text='A').place(x=120, y=70)
         eight_Label = Label(canvas, text='8').place(x=80, y=120)
@@ -232,5 +246,8 @@ def Start():
 def main():
     Start()
 
+
+
 if __name__ == '__main__': main()
+
 
--- Project1/main.py
+++ Project1/main.py
@@ -1,55 +1,198 @@
-from tkinter import *
-BASE_WINDOW_SIZE = '600x600'
-name_var = ""
-password_var = ""
+from tkinter import StringVar, Label, Entry, Button, Tk, Frame
+LOGIN_WINDOW_SIZE = '400x400'
+MAIN_WINDOW_SIZE = '600x600'
+from os import read, write
 
-class GUI:
-    def __init__(self, window):
-        self.window = window
+def main():
+    #Init_Login_GUI()
+    Init_CallLog_GUI()
+    #Init_Login_GUI_1()
+    #Hash_Password()
+    #Encrypt()
+    #Authenticate()
+    #Init_CallLog_Window()
 
-    def show_entry_fields(self, user, password):
+def Init_CallLog_GUI():
+    _gui = CallLog_GUI()
+    _gui.Init_Window()
 
-        print("First Name: %s\nLast Name: %s" % (user, password))
+
+def Init_Login_GUI():
+    login_win = Tk()
+    frame = Frame(login_win)
+    frame.pack()
+    gui = Login_GUI()
+    gui.create_input_boxes(frame)
+    gui.create_login_window(login_win)
+
+def Init_Login_GUI_1():
+    login_win = Tk()
+    frame = Frame(login_win)
+    frame.pack()
+    login_GUI = Login_GUI_02()
+    login_GUI.Init_GUI('600x600', 'Login Test')
+    login_GUI.GUI_Components()
 
 
-    def create_input_boxes(self, window, frame):
-        username_input = StringVar
-        password_input = StringVar
+class Login_GUI:
+    def __init__(self):
+        self.username_var = StringVar()
+        self.password_var = StringVar()
+
+    def create_input_boxes(self, frame):
         username_label = Label(frame, text="Username")
-        username_input = Entry(frame, width=40, textvariable=username_input)
+        username_input = Entry(frame, width=40, textvariable=self.username_var)
         password_label = Label(frame, text="Password")
-        password_input = Entry(frame, width=40, textvariable=password_input)
-        input_button = Button(frame, text="Submit", width="34", command=lambda: self.show_entry_fields())
-
+        password_input = Entry(frame, width=40, textvariable=self.password_var)
+        input_button = Button(frame, text="Submit", width="34", command=lambda: self.submit_details())
         username_label.pack()
         username_input.pack()
         password_label.pack()
         password_input.pack()
         input_button.pack()
 
-def create_input_boxes(window, frame):
-    gui = GUI(window)
-    gui.create_input_boxes(window, frame)
-    gui.show_entry_fields()
+    @staticmethod
+    def create_login_window(login_window):
+        login_window.geometry(LOGIN_WINDOW_SIZE)
+        login_window.title("Login Page")
+        login_window.mainloop()
 
-def create_main_window(window, frame):
-    window.geometry(BASE_WINDOW_SIZE)
-    window.title("Login Page")
-    window.mainloop()
+    def submit_details(self):
+        print('Submitted')
+        print("First Name: %s\nLast Name: %s" % (self.username_var.get(), self.password_var.get()))
 
-def Initalise_Login_GUI():
-    root = Tk()
-    frame = Frame(root)
 
-    frame.pack()
-    create_input_boxes(root, frame)
-    create_main_window(root, frame)
 
-def Authenticate():
+#______________________________________________________________________________
+
+
+class GUI:
+    def __init__(*args):
+        print()
+
+    @staticmethod
+    def Init_GUI(win_size, title):
+        GUI_window = Tk()
+        frame = Frame(GUI_window)
+        GUI_window.geometry(win_size)
+        GUI_window.title(title)
+
+    @staticmethod
+    def Init_Components(**kwargs):
+       pass
+
+    @staticmethod
+    def Init_Button(**kwargs):
+        _button = Button(kwargs)
+        _button.pack()
+
+    @staticmethod
+    def Init_Label(**kwargs):
+        _label = Label(kwargs)
+        _label.pack()
+
+    @staticmethod
+    def Init_TextBox(**kwargs):
+        _textbox = Entry(kwargs)
+        _textbox.pack()
+
+#_____________________________________________________________________
+
+
+class Login_GUI_02(GUI):
+    def __init__(self):
+        super().__init__()
+        self.username_var = StringVar()
+        self.password_var = StringVar()
+
+
+    def GUI_Components(self):
+        self.Init_GUI('800x800', 'Login Page')
+        self.Init_Label(text='TEST', width=40, height=10)
+
+        self.Init_Button(text='button', width=40, height=10)
+        self.Init_Button(text='Submit', width=34, command=lambda: self.submit_details())
+
+        self.Init_TextBox(width=40, height=10, textvarible=self.password_var)
+        self.Init_TextBox(width=40, height=10, textvariable=self.username_var)
+
+    def submit(self):
+        print(self.username_var, self.password_var)
+        pass
+
+    @staticmethod
+    def call_log_window(login_window):
+        login_window.geometry(LOGIN_WINDOW_SIZE)
+        login_window.title("Login Page")
+        login_window.mainloop()
+
+    def submit_details(self):
+        print('Submitted')
+        print("First Name: %s\nLast Name: %s" % (self.username_var.get(), self.password_var.get()))
+
+
+
+#_____________________________________________________________________________
+
+
+
+class CallLog_GUI:
+    def __init__(self):
+        pass
+
+    def Init_Window(self):
+        callLog_window = Tk()
+        frame = Frame(callLog_window)
+        callLog_window.geometry(MAIN_WINDOW_SIZE)
+        callLog_window.title('Call Log System')
+        self.Init_Components()
+        self.Init_GUI(callLog_window)
+
+
+    def Init_Components(self):
+        _label01 = Label(text='Ticket Search', width=15).grid(row=1, column=0)
+        _entry01 = Entry(width=30).grid(row=0, column=0)
+        _button01 = Button(text='View Current', width= 20, height=5, command=lambda: self.Current_Tickets())
+        _button02 = Button(text='Raise a Ticket', width= 20, height=5, command=lambda: self.Raise_Ticket())
+        _button03 = Button(text='Search Ticket', width= 10,height=0, command=lambda: self.Search_Tickets()).grid(row=0, column=1)
+
+    @staticmethod
+    def Init_GUI(gui_window):
+        gui_window.mainloop()
+
+    def Raise_Ticket(self):
+        print('Raise Ticket')
+        pass
+
+    def Current_Tickets(self):
+        print('View Tickets')
+        pass
+
+    def Search_Tickets(self):
+        print('Search Tickets')
+        pass
+
+
+
+
+def read_file():
+    path = 'C:/Project1/Authentication Names'
+    file = open('Authentication Names.txt', 'r')
+    lines = file.readline()
+    for line in lines:
+        if line == '':
+            break
+
+
+
+def Encrypt():
     pass
 
+def Authenticate():
+    read_file()
 
-
+def Hash_Password():
+    pass
 
 class Ticket:
     def __init__(self, user, department, email, issue_summary, issue_description, date, status, assigned):
@@ -64,10 +207,5 @@ class Ticket:
 
 
 
-def main():
-    Initalise_Login_GUI()
-    Authenticate()
-    print(name_var, password_var)
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': main()
 
